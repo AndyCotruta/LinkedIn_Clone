8feeb32955eafedc2b361e545c6e4cab0e4cac01
AndyCotruta <acotruta18@gmail.com>
2023-05-18 18:49:15 +0200

    working at navbar responsivness

--- src/components/Navbar/MainNavbar.jsx
+++ src/components/Navbar/MainNavbar.jsx
@@ -1,5 +1,4 @@
 import {
-  Navbar,
   Nav,
   NavDropdown,
   FormControl,
@@ -7,6 +6,7 @@ import {
   Button,
   Container,
 } from "react-bootstrap";
+import search from "../../Icon/Search.svg";
 import logomini from "../../Icon/Logo-nav.svg";
 import home from "../../Icon/Home.svg";
 import mynetwork from "../../Icon/myNetwork.svg";
@@ -56,13 +56,10 @@ const MainNavbar = ({ signUp, setSignUp }) => {
   }, [query]);
 
   return (
-    <Navbar expand="lg" className="navbar-main">
-      <Container className="px-0">
-        <div
-          className="d-flex justify-content-between"
-          style={{ width: "100%", paddingInline: "15px" }}
-        >
-          <div className="logo-on-collapse d-flex align-items-center ">
+    <div expand="lg" className="navbar-main">
+      <Container className="px-3">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex align-items-center ">
             <div
               className="logo-mini"
               onClick={() => {
@@ -72,7 +69,7 @@ const MainNavbar = ({ signUp, setSignUp }) => {
               <img src={logomini} alt="" />
             </div>
 
-            <Form inline className="search-position">
+            <Form inline className="search-container">
               <FormControl
                 type="text"
                 placeholder="Search"
@@ -96,218 +93,215 @@ const MainNavbar = ({ signUp, setSignUp }) => {
             </Form>
           </div>
 
-          <div className="menu-on-collapse">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="elapsed-menu mr-auto d-flex align-items-center">
-                <Nav.Link className="p-0">
-                  <div
-                    className=" d-flex flex-column align-items-center nav-icon menu-size"
-                    onClick={() => {
-                      navigate("/");
-                    }}
-                  >
-                    <div>
-                      <img src={home} alt="" className="nav-menu-icon" />
-                    </div>
-                    <p className="fs-12 nav-text">Home</p>
+          <div className="">
+            <div className="elapsed-menu mr-auto d-flex align-items-center">
+              <Nav.Link className="search-icon p-0">
+                <div className=" d-flex flex-column align-items-center nav-icon menu-size">
+                  <div>
+                    <img src={search} alt="" className="nav-menu-icon" />
                   </div>
-                </Nav.Link>
-                <Nav.Link className="p-0">
-                  <div className=" d-flex flex-column align-items-center nav-icon menu-size">
-                    <div>
-                      <img src={mynetwork} alt="" className="nav-menu-icon" />
-                    </div>
-                    <p className="fs-12 nav-text">My Network</p>
-                  </div>
-                </Nav.Link>
-                <Nav.Link className="p-0">
-                  <div className=" d-flex flex-column align-items-center nav-icon menu-size">
-                    <div>
-                      <img src={jobs} alt="" className="nav-menu-icon" />
-                    </div>
-                    <p className="fs-12 nav-text">Jobs</p>
-                  </div>
-                </Nav.Link>
-                <Nav.Link className="p-0">
-                  <div className=" d-flex flex-column align-items-center nav-icon menu-size">
-                    <div>
-                      <img src={message} alt="" className="nav-menu-icon" />
-                    </div>
-                    <p className="fs-12 nav-text">Messaging</p>
-                  </div>
-                </Nav.Link>
-                <Nav.Link className="p-0">
-                  <div className=" d-flex flex-column align-items-center nav-icon menu-size">
-                    <div>
-                      <img
-                        src={notification}
-                        alt=""
-                        className="nav-menu-icon"
-                      />
-                    </div>
-                    <p className="fs-12 nav-text">Notifications</p>
-                  </div>
-                </Nav.Link>
+                  <p className="fs-12 nav-text">Search</p>
+                </div>
+              </Nav.Link>
 
-                {myProfile && (
-                  <div className="profile-drop-down menu-size">
-                    <img
-                      src={myProfile.image}
-                      alt=""
-                      className="profile-icon"
-                    />
-                    <NavDropdown
-                      title="Me"
-                      id="basic-nav-dropdown"
-                      className="profile-name"
+              <Nav.Link className="p-0">
+                <div
+                  className=" d-flex flex-column align-items-center nav-icon menu-size"
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                >
+                  <div>
+                    <img src={home} alt="" className="nav-menu-icon" />
+                  </div>
+                  <p className="fs-12 nav-text">Home</p>
+                </div>
+              </Nav.Link>
+              <Nav.Link className="p-0">
+                <div className=" d-flex flex-column align-items-center nav-icon menu-size">
+                  <div>
+                    <img src={mynetwork} alt="" className="nav-menu-icon" />
+                  </div>
+                  <p className="fs-12 nav-text">My Network</p>
+                </div>
+              </Nav.Link>
+              <Nav.Link className="p-0">
+                <div className=" d-flex flex-column align-items-center nav-icon menu-size">
+                  <div>
+                    <img src={jobs} alt="" className="nav-menu-icon" />
+                  </div>
+                  <p className="fs-12 nav-text">Jobs</p>
+                </div>
+              </Nav.Link>
+              <Nav.Link className="p-0">
+                <div className=" d-flex flex-column align-items-center nav-icon menu-size">
+                  <div>
+                    <img src={message} alt="" className="nav-menu-icon" />
+                  </div>
+                  <p className="fs-12 nav-text">Messaging</p>
+                </div>
+              </Nav.Link>
+              <Nav.Link className="p-0">
+                <div className=" d-flex flex-column align-items-center nav-icon menu-size">
+                  <div>
+                    <img src={notification} alt="" className="nav-menu-icon" />
+                  </div>
+                  <p className="fs-12 nav-text">Notifications</p>
+                </div>
+              </Nav.Link>
+
+              {myProfile && (
+                <div className="profile-drop-down menu-size">
+                  <img src={myProfile.image} alt="" className="profile-icon" />
+                  <NavDropdown
+                    title="Me"
+                    id="basic-nav-dropdown"
+                    className="profile-name"
+                  >
+                    <NavDropdown.Item
+                      className="dropdown-main-action d-flex align-items-center"
+                      onClick={() => {
+                        navigate(`/profile/${myProfile._id}`);
+                        dispatch({
+                          type: ADD_CLICKED_PROFILE,
+                          payload: myProfile,
+                        });
+                      }}
                     >
-                      <NavDropdown.Item
-                        className="dropdown-main-action d-flex align-items-center"
+                      <span>
+                        <img
+                          src={myProfile.image}
+                          alt=""
+                          className="profile-icon2 mr-2"
+                        />
+                      </span>
+                      <div style={{ color: "black" }}>
+                        <p className="fs-16 fw-700">
+                          {myProfile.name} {myProfile.surname}
+                        </p>
+                        <p className="fs-14 ">{myProfile.title}</p>
+                      </div>
+                    </NavDropdown.Item>
+                    <div
+                      href="#action/3.1.2"
+                      className="dropdown-secondary-action d-flex justify-content-center mt-2 mb-2"
+                    >
+                      <Button
+                        className="btn nav-view-profile-btn fs-14"
                         onClick={() => {
                           navigate(`/profile/${myProfile._id}`);
-                          dispatch({
-                            type: ADD_CLICKED_PROFILE,
-                            payload: myProfile,
-                          });
                         }}
                       >
-                        <span>
-                          <img
-                            src={myProfile.image}
-                            alt=""
-                            className="profile-icon2 mr-2"
-                          />
-                        </span>
-                        <div style={{ color: "black" }}>
-                          <p className="fs-16 fw-700">
-                            {myProfile.name} {myProfile.surname}
-                          </p>
-                          <p className="fs-14 ">{myProfile.title}</p>
-                        </div>
-                      </NavDropdown.Item>
-                      <div
-                        href="#action/3.1.2"
-                        className="dropdown-secondary-action d-flex justify-content-center mt-2 mb-2"
-                      >
-                        <Button
-                          className="btn nav-view-profile-btn fs-14"
-                          onClick={() => {
-                            navigate(`/profile/${myProfile._id}`);
-                          }}
-                        >
-                          View Profile
-                        </Button>
-                      </div>
-                      <NavDropdown.Divider />
-                      <div
-                        href="#action/3.2"
-                        className="dropdown-main-action fs-16 fw-800 ml-4"
-                      >
-                        Account
-                      </div>
-                      <NavDropdown.Item
-                        href="#action/3.2.1"
-                        className="dropdown-secondary-action fs-14"
-                      >
-                        Try premium for free
-                      </NavDropdown.Item>
-                      <NavDropdown.Item
-                        href="#action/3.2.2"
-                        className="dropdown-secondary-action fs-14"
-                      >
-                        Settings & Privacy
-                      </NavDropdown.Item>
-                      <NavDropdown.Item
-                        href="#action/3.2.3"
-                        className="dropdown-secondary-action fs-14"
-                      >
-                        Help
-                      </NavDropdown.Item>
-                      <NavDropdown.Item
-                        href="#action/3.2.4"
-                        className="dropdown-secondary-action fs-14"
-                      >
-                        Language
-                      </NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <div
-                        href="#action/3.3"
-                        className="dropdown-main-action fs-16 fw-800 ml-4"
-                      >
-                        Manage
-                      </div>
-                      <NavDropdown.Item
-                        href="#action/3.3.1"
-                        className="dropdown-secondary-action fs-14"
-                      >
-                        Posts & Activity
-                      </NavDropdown.Item>
-                      <NavDropdown.Item
-                        href="#action/3.3.2"
-                        className="dropdown-secondary-action fs-14"
-                      >
-                        Job Posting Account
-                      </NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item
-                        className="fs-14"
-                        onClick={() => {
-                          handleSignOut();
-                        }}
-                      >
-                        Sign Out
-                      </NavDropdown.Item>
-                    </NavDropdown>
-                  </div>
-                )}
-                {myProfile ? (
-                  <div className="d-flex">
-                    <div className="profile-drop-down menu-size work">
-                      <img src={work} alt="" className="nav-menu-icon" />
-                      <NavDropdown
-                        title="Work"
-                        id="basic-nav-dropdown"
-                        className="profile-name "
-                      ></NavDropdown>
+                        View Profile
+                      </Button>
                     </div>
-                    <Nav.Link href="#premium" className="text-align-center ">
-                      <p className="fs-12 fw-700 premium">Network Smarter,</p>{" "}
-                      <p className="fs-12 fw-700 premium">Try premium Free!</p>
-                    </Nav.Link>
-                  </div>
-                ) : (
-                  <div className="login-buttons">
+                    <NavDropdown.Divider />
                     <div
+                      href="#action/3.2"
+                      className="dropdown-main-action fs-16 fw-800 ml-4"
+                    >
+                      Account
+                    </div>
+                    <NavDropdown.Item
+                      href="#action/3.2.1"
+                      className="dropdown-secondary-action fs-14"
+                    >
+                      Try premium for free
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      href="#action/3.2.2"
+                      className="dropdown-secondary-action fs-14"
+                    >
+                      Settings & Privacy
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      href="#action/3.2.3"
+                      className="dropdown-secondary-action fs-14"
+                    >
+                      Help
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      href="#action/3.2.4"
+                      className="dropdown-secondary-action fs-14"
+                    >
+                      Language
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <div
+                      href="#action/3.3"
+                      className="dropdown-main-action fs-16 fw-800 ml-4"
+                    >
+                      Manage
+                    </div>
+                    <NavDropdown.Item
+                      href="#action/3.3.1"
+                      className="dropdown-secondary-action fs-14"
+                    >
+                      Posts & Activity
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      href="#action/3.3.2"
+                      className="dropdown-secondary-action fs-14"
+                    >
+                      Job Posting Account
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item
+                      className="fs-14"
                       onClick={() => {
-                        setSignUp(true);
+                        handleSignOut();
                       }}
                     >
-                      {signUp ? (
-                        <BlueButton text={"Sign Up"} />
-                      ) : (
-                        <BlueBorderBtn content={"Sign Up"} />
-                      )}
-                    </div>
-                    <div
-                      onClick={() => {
-                        setSignUp(false);
-                      }}
-                    >
-                      {signUp ? (
-                        <BlueBorderBtn content={"Log In"} />
-                      ) : (
-                        <BlueButton text={"Log In"} />
-                      )}
-                    </div>
+                      Sign Out
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
+              )}
+              {myProfile ? (
+                <div className="d-flex align-items-center">
+                  <div className="profile-drop-down menu-size work">
+                    <img src={work} alt="" className="nav-menu-icon" />
+                    <div className="work-text">Work</div>
                   </div>
-                )}
-              </Nav>
-            </Navbar.Collapse>
+
+                  <div className="fs-12 fw-700 premium">
+                    <div>Network Smarter,</div> <div>Try premium Free!</div>
+                  </div>
+                </div>
+              ) : (
+                <div className="login-buttons">
+                  <div
+                    onClick={() => {
+                      setSignUp(true);
+                    }}
+                  >
+                    {signUp ? (
+                      <BlueButton text={"Sign Up"} />
+                    ) : (
+                      <BlueBorderBtn content={"Sign Up"} />
+                    )}
+                  </div>
+                  <div
+                    onClick={() => {
+                      setSignUp(false);
+                    }}
+                  >
+                    {signUp ? (
+                      <BlueBorderBtn content={"Log In"} />
+                    ) : (
+                      <BlueButton text={"Log In"} />
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
+        <div className="search-small-screens">
+          Search Input for smaller screens
+        </div>
       </Container>
-    </Navbar>
+    </div>
   );
 };
 export default MainNavbar;
